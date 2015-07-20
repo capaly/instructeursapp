895c242cee9a2efbf60c20576bd4458cdd845784
Rik Dekyvere <info@roundcone.com>
2015-07-20 11:12:13 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = 'server12.000webhost.com';
-$dbuser = 'a1542242_rikdlc';
-$dbpass = 'tandarts6787';
-$dbname = 'a1542242_iehbo';
+$dbhost = 'xxx';
+$dbuser = 'xxx';
+$dbpass = 'xxx';
+$dbname = 'xxx';
 
 
 
